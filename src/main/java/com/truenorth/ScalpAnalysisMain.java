701a9df50f4394467817aa7fe0f4032b94a85da3
Brian Northan <bnorthan@gmail.com>
2016-02-06 10:52:03 -0500

    Set fiji plugin path

--- src/main/java/com/truenorth/ScalpAnalysisMain.java
+++ src/main/java/com/truenorth/ScalpAnalysisMain.java
@@ -8,11 +8,14 @@ import net.imglib2.type.numeric.RealType;
 
 public class ScalpAnalysisMain {
 	public static <T extends RealType<T>> void main(final String... args) throws Exception {
-		// create the ImageJ application context with all available services
-		final ImageJ ij = new ImageJ();// net.imagej.Main.launch(args);
-
-		System.out.println(Class.forName("fiji.threshold.Auto_Local_Threshold"));
-
+		
+		System.setProperty("plugins.dir", "/home/bnorthan/fiji/Fiji.app");
+		
+		// comment this in to show imagej gui
+		//final ImageJ ij=net.imagej.Main.launch(args);
+		// or this one for headless imagej
+		final ImageJ ij=new ImageJ();
+	
 		String imName = "/home/bnorthan/images/ij_list/hair transplant/aR5VYcropped.tif";
 
 		final Dataset data = ij.dataset().open(new File(imName).getAbsolutePath());
